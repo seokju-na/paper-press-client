180341f9ce56e0511b2ea9c3d2aaa0fd4ebe3cbc
Seokju Na <seokmaTD@gmail.com>
2016-01-28 19:19:47 +0900

    [webpack] initialize webpack.config.js
    Use 'hjs-webpack' plugin to compile sources.

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,8 @@
-var app;
+require('babel-register');
+var getConfig = require('hjs-webpack');
+
+module.exports = getConfig({
+    in: 'src/main.js',
+    out: 'dist',
+    clearBeforeBuild: true
+});
